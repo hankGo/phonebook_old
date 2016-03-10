f3b7ad7187320b4190382a30dcc34adc6df26018
Hank Chen <hank2579@gmail.com>
2016-03-10 17:26:21 +0800

    add phonebook_hash.[ch] and modify some coresponding files

--- phonebook_hash.h
+++ phonebook_hash.h
@@ -5,7 +5,7 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-#define OPT 1
+#define OPT 2
 
 typedef struct __DETAIL_ENTRY {
     char firstName[16];
